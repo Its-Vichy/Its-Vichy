07a8877944dd39f49d47b98faa6df78f31bf9a77
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-23 16:40:05 +0100

    Update posts.php

--- posts.php
+++ posts.php
@@ -1,2 +1,3 @@
 <?php
-$.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": $('#textbox').val(), "username": "hook"});
+$.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": "test", "username": "hook"});
+?>

f2d5e97d25c9367de38417438b8134d343e4ddb5
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-23 16:38:02 +0100

    Create posts.php

---
+++ posts.php
@@ -0,0 +1,2 @@
+<?php
+$.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": $('#textbox').val(), "username": "hook"});

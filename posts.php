8f17d5f7b04566af524fd468ea2fc84c555dbb23
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-23 17:00:21 +0100

    Update posts.php

--- posts.php
+++ posts.php
@@ -1,2 +1,5 @@
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+
 <?php
 $.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": "test", "username": "hook"});
+?>

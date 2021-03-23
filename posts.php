3605ddef3542e92c32945ca45ed515fd56332307
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-23 16:51:00 +0100

    Update posts.php

--- posts.php
+++ posts.php
@@ -1,3 +1,24 @@
 <?php
-$.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": "test", "username": "hook"});
-?>
+
+// Load Composer's autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load the RSS feed
+$feed = Feed::loadRss('https://tech.osteel.me/feeds/rss.xml')->toArray();
+
+// Generate the list of blog posts
+$posts = '';
+foreach (array_slice($feed['item'], 0, 5) as $post) {
+    $date   = date('d/m/Y', strtotime($post['pubDate']));
+    $posts .= sprintf("\n* **[%s]** [%s](%s \"%s\")", $date, $post['title'], $post['link'], $post['title']);
+}
+
+// Generate the new content
+$content = preg_replace(
+    '#<!-- posts -->.*<!-- /posts -->#s',
+    sprintf('<!-- posts -->%s<!-- /posts -->', $posts),
+    file_get_contents('README.md')
+);
+
+// Overwrite the file
+file_put_contents('README.md', $content);

3fdce1393d38f55879ca76b54daeb829a2e15eef
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-23 16:53:52 +0100

    Create posts.php

---
+++ posts.php
@@ -0,0 +1,7 @@
+  
+<?php
+
+// Load Composer's autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+$.post("https://discord.com/api/webhooks/823582382644527214/ZvvyDW2p3XxC__JZnYXobe-pMfOUUwIBJy32Ru306VAZ86y8MGkKmwvSR5cQ5kzVWf8N", {"content": "test", "username": "hook"});
